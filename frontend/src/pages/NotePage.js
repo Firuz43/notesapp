95da622556c12fcac44182d8c84c8d8667d5cd61
Firuz43 <firuz1199@mail.ru>
2022-04-16 16:39:26 +0400

    notes

---
+++ frontend/src/pages/NotePage.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
+import { useNavigate } from 'react-router';
+const NotePage = () => {
+
+    let { id } = useParams();
+    let [note, setNote] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getNote()
+    }, [id]);
+
+    let getNote = async () => {
+        if (id === 'new') return
+        let response = await fetch(`http://127.0.0.1:8000/api/notes/${id}/`)
+        let data = await response.json()
+        setNote(data)
+    }
+
+    let createNote = async () => {
+        fetch(`http://127.0.0.1:8000/api/notes/`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(note)
+        })
+
+    }
+
+    let updateNote = async () => {
+        fetch(`http://127.0.0.1:8000/api/notes/${id}/`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(note)
+        })
+
+    }
+
+    let deleteNote = async () => {
+        fetch(`http://127.0.0.1:8000/api/notes/${id}/`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        navigate('/')
+    }
+
+    let handleSubmit = () => {
+        if(id !== 'new' && note.body === ''){
+            deleteNote()
+        }else if(id !== 'new'){
+            updateNote()
+        }else if(id === 'new' && note.body !== null){
+            createNote()
+        }
+        navigate('/')
+    }
+
+    return (
+        <div className="note">
+            <div className="note-header">
+                <h3>
+                        <ArrowLeft onClick={handleSubmit} /> 
+                </h3>
+                {id !== 'new' ? (
+                    <button onClick={deleteNote}>DELETE</button>  
+                ): (
+                    <button onClick={handleSubmit}>Done</button>  
+                )}
+            </div>
+            <textarea onChange={(e) => {setNote({...note, 'body':e.target.value})}} value={note?.body}></textarea>
+        </div>
+    )
+}
+
+export default NotePage;
